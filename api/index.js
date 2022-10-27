5019889ef0e3def542f532c07a97c239aa29dcd4
abhxnav <k.abhinav1000@gmail.com>
2022-10-27 20:56:43 +0530

    authentication model complete

--- api/index.js
+++ api/index.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(console.log('Connected to MongoDB'))
   .catch((err) => {
     console.log(err);
   });
+
+app.use('/api/auth', authRoute);
 
 app.listen('3000', () => {
   console.log('Backend is running');
