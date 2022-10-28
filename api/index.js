95769fc4b0c1ab6832136f4ae173349e7554b461
abhxnav <k.abhinav1000@gmail.com>
2022-10-28 17:54:17 +0530

    multer setup; backend complete

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
 const categoryRoute = require('./routes/categories');
+const multer = require('multer');
 
 dotenv.config();
 app.use(express.json());
@@ -19,6 +20,20 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('File Uploaded Successfully!');
+});
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
