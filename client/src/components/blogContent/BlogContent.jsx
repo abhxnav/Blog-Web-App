6ec7fcd186c6786047b19afadbc9a2967096a063
abhxnav <k.abhinav1000@gmail.com>
2022-11-02 18:48:18 +0530

    Update Blog module complete

--- client/src/components/blogContent/BlogContent.jsx
+++ client/src/components/blogContent/BlogContent.jsx
@@ -8,14 +8,19 @@ import './blogContent.scss';
 const BlogContent = () => {
   const location = useLocation();
   const path = location.pathname.split('/')[2];
-  const [post, setPost] = useState({});
   const PF = 'http://localhost:5000/images/';
   const { user } = useContext(Context);
+  const [post, setPost] = useState({});
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  const [updatePost, setUpdatePost] = useState(false);
 
   useEffect(() => {
     const fetchPost = async () => {
       const res = await axios.get(`/posts/${path}`);
       setPost(res.data);
+      setTitle(res.data.title);
+      setDesc(res.data.desc);
     };
     fetchPost();
   }, [path]);
@@ -38,29 +43,85 @@ const BlogContent = () => {
     }
   };
 
+  const handleUpdate = async () => {
+    if (window.confirm('Do you really want to update this blog?'))
+      try {
+        await axios.put('/posts/' + path, {
+          username: user.username,
+          title,
+          desc,
+        });
+        setUpdatePost(false);
+        alert('Blog Updated!');
+      } catch (error) {
+        console.log(error);
+      }
+  };
+
   return (
     <div className="blog-content">
       <div className="blog-content-wrapper">
-        {post.photo && <img src={PF + post.photo} alt="blogContent" className="blog-content-img" />}
-        <h1 className="blog-content-title">
-          {post.title}
-          {post.username === user?.username && (
-            <div className="blog-content-edit">
-              <i className="blog-content-icons fa-solid fa-pen-to-square"></i>
-              <i className="blog-content-icons fa-solid fa-trash" onClick={handleDelete}></i>
-            </div>
-          )}
-        </h1>
+        {post.photo && (
+          <img
+            src={PF + post.photo}
+            alt="blogContent"
+            className="blog-content-img"
+          />
+        )}
+        {updatePost ? (
+          <input
+            type="text"
+            value={title}
+            className="blog-content-title-input"
+            autofocus={true}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        ) : (
+          <h1 className="blog-content-title">
+            {title}
+            {post.username === user?.username && (
+              <div className="blog-content-edit">
+                <i
+                  className="blog-content-icons fa-solid fa-pen-to-square"
+                  onClick={() => setUpdatePost(true)}
+                ></i>
+                <i
+                  className="blog-content-icons fa-solid fa-trash"
+                  onClick={handleDelete}
+                ></i>
+              </div>
+            )}
+          </h1>
+        )}
         <div className="blog-content-info">
           <span className="blog-content-author">
             Author :
-            <Link to={`/?user=${post.username}`} className="link" style={{ marginLeft: '5px' }}>
+            <Link
+              to={`/?user=${post.username}`}
+              className="link"
+              style={{ marginLeft: '5px' }}
+            >
               <b>{post.username}</b>
             </Link>
           </span>
-          <span className="blog-content-date">{new Date(post.createdAt).toDateString()}</span>
+          <span className="blog-content-date">
+            {new Date(post.createdAt).toDateString()}
+          </span>
         </div>
-        <p className="blog-content-description">{post.desc}</p>
+        {updatePost ? (
+          <textarea
+            className="blog-content-description-input"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        ) : (
+          <p className="blog-content-description">{desc}</p>
+        )}
+        {updatePost && (
+          <button className="edit-submit-button" onClick={handleUpdate}>
+            Update
+          </button>
+        )}
       </div>
     </div>
   );
